c8ecf8aaf967f46240a2ee24105b245814fe28da
Migle224 <mpucetaite@gmail.com>
2018-10-05 23:00:51 +0100

    Added Book Service entities

---
+++ miglavandeny/Services/BookServices/BookService.Data/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookService.Data
+{
+    public class Author
+    {
+        public Guid Id;
+        public string Name;
+        public string Surname;
+        public DateTime CreatedOn;
+
+        public List<Book> Books;
+    }
+}
---
+++ miglavandeny/Services/BookServices/BookService.Data/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookService.Data
+{
+    public class Book
+    {
+        public Guid Id;
+        public string Name;
+        public string Description;
+        public Guid StatusId;
+        public DateTime? LastReadTime;
+        public DateTime CreatedOn;
+
+        public Status Status;
+        public List<Author> Authors;
+    }
+}
